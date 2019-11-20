436fc7e2a60bdaa67304e432a50d6de4317738f6
zhaoyu <zhaoyu@qingsongchou.com>
2019-11-20 15:08:27 +0800

    go mod

--- app/main.go
+++ app/main.go
@@ -1,9 +1,16 @@
 package main
 
-
 import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main(){
+    r := gin.Default()
+    r.GET("/ping", func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "message": "pong",
+        })
+    })
+    r.Run()
     fmt.Println("赵宇大好人111")
 }
 
