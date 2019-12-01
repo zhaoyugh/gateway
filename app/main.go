f319894ac66c10223998f80a4af1a75646cad549
zhaoyu <zhaoyu@qingsongchou.com>
2019-12-01 17:16:27 +0800

    ttt

--- app/main.go
+++ app/main.go
@@ -4,7 +4,7 @@ import "fmt"
 import "github.com/gin-gonic/gin"
 
 func main(){
-    fmt.Println("赵宇大好人11223332")
+    fmt.Println("赵宇大好人1122ee3332")
     r := gin.Default()
     r.GET("/ping", func(c *gin.Context) {
         c.JSON(200, gin.H{
