72b87e8e65b56433baf62cfee232f878b58f0a64
zhaoyu <zhaoyu@qingsongchou.com>
2019-12-06 14:32:00 +0800

    ttt

--- app/main.go
+++ app/main.go
@@ -8,7 +8,7 @@ func main(){
     r := gin.Default()
     r.GET("/ping", func(c *gin.Context) {
         c.JSON(200, gin.H{
-            "message": "pong123",
+            "message": "赵宇大好人",
         })
     })
     r.Run(":8081")
